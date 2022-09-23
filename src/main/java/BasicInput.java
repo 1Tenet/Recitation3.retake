979131f07cac25a7ee2a794cf0abcd674bac4179
1Tenet <97625761+1tenet@users.noreply.github.com>
2022-09-23 14:11:47 -0400

    First commit

--- src/main/java/BasicInput.java
+++ src/main/java/BasicInput.java
@@ -5,11 +5,19 @@ public class BasicInput {
         Scanner scnr = new Scanner(System.in);
         int userInt = 0;
         double userDouble = 0.0;
+<<<<<<< HEAD
+=======
+        char userChar;
+        String userString;
+        String str, rev = "";
+
+>>>>>>> 5aae45e (First commit)
         // TODO Define char and string variables similarly
 
         System.out.println("Enter integer:");
         userInt = scnr.nextInt();
 
+<<<<<<< HEAD
         // TODO (1): Finish reading other items into variables, then output the four values on a single line separated by a space
 
         // TODO (2): Output the four values in reverse
@@ -17,5 +25,29 @@ public class BasicInput {
         // TODO (3): Cast the double to an integer, and output that integer
 
         return;
+=======
+        System.out.println("Enter double:");
+        userDouble = scnr.nextDouble();
+
+        System.out.println("Enter character: ");
+        userChar = scnr.next().charAt(0);
+
+        System.out.println("Enter string:");
+        userString = scnr.next();
+        System.out.println(userInt + " " + userDouble + " " + userChar + " " + userString);
+        System.out.println(userString + " " + userChar + " " + userDouble + " " + userInt);
+
+
+        System.out.println(userDouble+" cast to an integer is " + (int)userDouble);
+
+
+
+                // TODO (1): Finish reading other items into variables, then output the four values on a single line separated by a space
+
+                // TODO (2): Output the four values in reverse
+
+                // TODO (3): Cast the double to an integer, and output that integer
+
+>>>>>>> 5aae45e (First commit)
     }
 }
